0844565d992cedba7481d2ec52a88d9b579e0df2
Collin Haney <collin.haney@yahoo.com>
2022-04-07 18:57:59 -0500

    Typo fix and added sentence

--- src/Checkout.tsx
+++ src/Checkout.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Checkout.css'
 
 const Checkout = () => {
+
+
   return (
     <div>
       <h1>Checkout product</h1>
+      <p className="main-p">Hello world!</p>
       <button>One-click barter</button>
     </div>
   );
