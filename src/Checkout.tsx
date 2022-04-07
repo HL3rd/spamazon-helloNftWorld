df0500221e1c2a33d08a4b5dd8feb617fd2fffab
Horacio Lopez <hlopez3rd@gmail.com>
2022-04-07 15:55:30 -0500

    Literally the bare minimum

---
+++ src/Checkout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Checkout = () => {
+  return (
+    <div>
+      <h1>Checkout product</h1>
+      <button>One-click barter</button>
+    </div>
+  );
+}
+
+export default Checkout;
