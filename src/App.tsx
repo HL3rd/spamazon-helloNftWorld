9f3e7c35ef9fa56e72fa567d6b25b24b1a6fe335
Horacio Lopez <hlopez3rd@gmail.com>
2022-04-08 02:19:00 -0500

    Wallet connect and displays NFTS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
 
 import Home from './Home';
 import Checkout from './Checkout';
--- src/Checkout.tsx
+++ src/Checkout.tsx
@@ -1,10 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ethers } from 'ethers';
 
-const Checkout = () => {
+import NFTContainer from './NFTContainer';
+
+const Checkout:React.FC = () => {
+
+  const testWalletAddress = "0x60f80121c31a0d46b5279700f9df786054aa5ee5"
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [userBalance, setUserBalance] = useState('');
+  const [nfts, setNfts] = useState([]);
+
+  const connectWallet = () => {
+    if ((window as any).ethereum) {
+      (window as any).ethereum.request({ method: 'eth_requestAccounts'})
+        .then((accounts:any) => {
+          accountChangedHandler(accounts[0]);
+        })
+    } else {
+      setErrorMessage('Please install Metamask');
+    }
+  }
+
+  const accountChangedHandler = (newAccount:any) => {
+    setWalletAddress(newAccount);
+    getUserBalance(newAccount.toString());
+    getNFTData(newAccount);
+  }
+
+  const getUserBalance = (address:any) => {
+    (window as any).ethereum.request({method: 'eth_getBalance', params: [address, 'latest']})
+      .then((balance:any) => {
+          setUserBalance(ethers.utils.formatEther(balance));
+      })
+  }
+
+  const getNFTData = async(address:any) => {
+
+    const response = await fetch(`https://api.rarible.org/v0.1/items/byOwner/?owner=ETHEREUM:${testWalletAddress}`);
+
+    const data = await response.json();
+
+    setNfts(data.items);
+  }
+
+  const chainChangedHandler = (newChain:any) => {
+    window.location.reload();
+  }
+
+  // Check if user changes their connected account
+  (window as any).ethereum.on('accountChanged', accountChangedHandler);
+
+  // If user changes testnets or test to devnet, reload site
+  (window as any).ethereum.on('chainChanged', chainChangedHandler);
+
   return (
     <div>
       <h1>Checkout product</h1>
-      <button>One-click barter</button>
+      <button onClick={connectWallet}>One-click barter</button>
+      <div>
+        <h3>Address: {walletAddress}</h3>
+      </div>
+      <div>
+        <h3>Balance: { userBalance }</h3>
+        <p>{errorMessage}</p>
+      </div>
+      <NFTContainer nfts={nfts} />
     </div>
   );
 }
