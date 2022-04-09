eab22dd7842eeffe961882b6e0fd1de42fb4f803
Collin Haney <collin.haney@yahoo.com>
2022-04-09 15:42:23 -0500

    Added gif and added more html and css

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './Home.css'
+import './Home.css';
+
+import Shark from "./assets/Shark.gif"
 
 const Home = () => {
   return (
@@ -9,6 +11,7 @@ const Home = () => {
         <h2 className="title-h1">Welcome home</h2>
         <p><a href="/checkout">Click here to view our product</a></p>
       </div>
+      <img alt="moving waves and shark fin" className="fix" src={Shark}></img>
     </body>
     
   );
