2f7d87cb70e84c986f9ffb14202e9470b379404a
Matteocos40 <77371622+Matteocos40@users.noreply.github.com>
2022-04-08 20:24:46 -0400

    added contract and config json to front_end

---
+++ NFTProjects/Hackathon-Brownie-UI/front_end/src/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome home</h1>
+      <p><a href="/checkout">Click here to view our product</a></p>
+    </div>
+  );
+}
+
+export default Home;
