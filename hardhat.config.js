d497edbdc3de78a2d85a9f5a17bad7926139fd8f
Horacio Lopez <hlopez3rd@gmail.com>
2022-04-09 23:18:55 -0500

    Final commit before really diving in

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,15 +3,15 @@
  */
  require('dotenv').config();
  require("@nomiclabs/hardhat-ethers");
- const { REACT_APP_ALCHEMY_KEY, REACT_APP_METAMASK_KEY } = process.env;
+ const { REACT_APP_ALCHEMY_URL, REACT_APP_PRIVATE_KEY } = process.env;
  module.exports = {
     solidity: "0.7.0",
     defaultNetwork: "rinkeby",
     networks: {
        hardhat: {},
        rinkeby: {
-          url: REACT_APP_ALCHEMY_KEY,
-          accounts: [`0x${REACT_APP_METAMASK_KEY}`]
+          url: REACT_APP_ALCHEMY_URL,
+          accounts: [`0x${REACT_APP_PRIVATE_KEY}`]
        }
     },
  }
