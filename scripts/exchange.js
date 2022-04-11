f176804f1e5c5db00bfec3617001c492d7d58000
Horacio Lopez <hlopez3rd@gmail.com>
2022-04-10 20:31:53 -0500

    scripts/exchange.js contains example for exchanging NFTs

--- scripts/exchange.js
+++ scripts/exchange.js
@@ -19,41 +19,23 @@ const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
 const barterContract = new ethers.Contract(BARTER_CONTRACT_ADDRESS, contract.abi, signer);
 
 async function main() {
-
-  // Need:
-  /**
-   * Need:
-   * - Test seller address to send: 0x6E9190A074371C3A1b96120D2b1A3834D9d51F64
-   * - Buyer address (currentWallet: 0x52554BfE4baC4aE605Af27A2e131480F2D219Fe6)
-   * - NFTContractAddress
-   * - TokenID
-   * function exchangeNFT(
-        address _buyer,
-        address _seller,
-        address _collection, 
-        uint256 _tokenID
-   * )
-   * 
-   */
-
-  const testBuyerAddr = "0x52554BfE4baC4aE605Af27A2e131480F2D219Fe6";
-  const testSellerAddr = "0x6E9190A074371C3A1b96120D2b1A3834D9d51F64";
-  const testNftContractAddr = "0x08207fe7f1f7c9f1c39e4720b9f7bfe2afd01907";
-  const testTokenId = 1; // Life is good
-
-  // Approve trade before attempting exchange
-  const ierc721Abi = IERC721Contract.abi;
-  const ierc721Interface = new ethers.utils.Interface(ierc721Abi);
-  const ierc721Contract = new ethers.Contract(testNftContractAddr, ierc721Abi, signer);
-
-  const firsty = await ierc721Contract.approve(BARTER_CONTRACT_ADDRESS, testTokenId, {from: testBuyerAddr});
-  console.log(`Go this first approval transaction: ${JSON.stringify(firsty)}`);
   
-  console.log(`Sending NFT: ${testNftContractAddr}, id ${testTokenId} over to ${testSellerAddr}`);
-  const tx = await barterContract.exchangeNFT(testBuyerAddr, testSellerAddr, testNftContractAddr, testTokenId, {from: testBuyerAddr, gasLimit: 5000000});
-  await tx.wait();
+  const testBuyerAddr = "0x52554BfE4baC4aE605Af27A2e131480F2D219Fe6"; // transfers the NFT token
+  const testNftContractAddr = "0x08207fe7f1f7c9f1c39e4720b9f7bfe2afd01907"; // NFT contract address
+  const testTokenId = 8; // NFT token id
+  const testSellerAddr = "0x6E9190A074371C3A1b96120D2b1A3834D9d51F64"; // receives the NFT token
+  
+  // Approve trade before attempting exchange using OpenZeppelin
+  // OpenZeppelin IERC721 functions are embedded in NFT contract so the ABI is functional
+  const ierc721Abi = IERC721Contract.abi;
+  const ierc721Contract = new ethers.Contract(testNftContractAddr, ierc721Abi, signer);
+  const tx_1 = await ierc721Contract.approve(BARTER_CONTRACT_ADDRESS, testTokenId);
+  console.log(`approve transaction: ${tx_1.hash}`);
+  
+  // Barter.sol, now approved, trigger the NFT exchange
+  const tx_2 = await barterContract.exchangeNFT(testBuyerAddr, testSellerAddr, testNftContractAddr, testTokenId, {from: testBuyerAddr, gasLimit: 5000000});
+  console.log(`exchangeNFT transaction: ${tx_2.hash}`);
   console.log(`Should now check ${testSellerAddr} to see if NFT ${testNftContractAddr} with tokenId ${testTokenId} transferred`); 
-
 }
 
 main()
--- src/NFTCard.tsx
+++ src/NFTCard.tsx
@@ -9,9 +9,8 @@ const NFTCard:React.FC<NFTCardProps> = ({ nft }) => {
   return (
     <div>
       <img alt="nft-img" src={nft.image_url} />
-      <p>ID: {nft.id}</p>
       <p>Collection: {nft.collection.slug}</p>
-      <p>Contract: {nft.asset_contract.address}</p>
+      <p>Contract: {nft.asset_contract.address}, tokenId: {nft.token_id}</p>
       <p>Name: {nft.name}</p>
       <p>{nft.description}</p>
     </div>
