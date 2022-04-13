abac8aae61acd31f5355a6718f04051f80481a04
Collin Haney <collin.haney@yahoo.com>
2022-04-13 17:05:16 -0500

    Fixing images and overlay

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,43 +4,45 @@ import './Home.css'
 const Home = () => {
   return (
     <body>
-      <div className="top">
-        <a href="/">
-          <div>
-            <h1 className="brand">SPAMAZON</h1>
-          </div>
-        </a>
-        
-        <a href="/checkout">
-          <div>
-            <h2 className="checkout">Checkout</h2>
-          </div>
-        </a>
-      </div>
+      <div className="whole-top">
+        <div className="top">
+          <a href="/">
+            <div>
+              <h1 className="brand">SPAMAZON</h1>
+            </div>
+          </a>
+          
+          <a href="/checkout">
+            <div>
+              <h2 className="checkout">Checkout</h2>
+            </div>
+          </a>
+        </div>
 
-      <div className="second-top">
-        <h3>Spam</h3>
-        <h3>Spam</h3>
-        <h3>Spam</h3>
-        <h3>Spam</h3>
-        <h3>Spam</h3>
-        <h3>Spam</h3>
-        <h3>Spam</h3>
-        <h3>Spam</h3>
-        <h3>Spam</h3>
-        <h3>Spam</h3>
-        <h3>Spam</h3>
-        <h3>Spam</h3>
-        <h3>Spam</h3>
-      </div>
+        <div className="second-top">
+          <h3>Spam</h3>
+          <h3>Spam</h3>
+          <h3>Spam</h3>
+          <h3>Spam</h3>
+          <h3>Spam</h3>
+          <h3>Spam</h3>
+          <h3>Spam</h3>
+          <h3>Spam</h3>
+          <h3>Spam</h3>
+          <h3>Spam</h3>
+          <h3>Spam</h3>
+          <h3>Spam</h3>
+          <h3>Spam</h3>
+        </div>
 
-      <div className="third-top">
-        <h2></h2>
+        <div className="fourth-top">
+          <h2>Welcome home</h2>
+        </div>
       </div>
 
 
       <div>
-        <h2 className="title">Welcome home</h2>
+        {/* <h2 className="title">Welcome home</h2> */}
         <p><a href="/checkout">Click here to view our product</a></p>
       </div>
     </body>
--- src/NFTCard.tsx
+++ src/NFTCard.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Product } from './constants/class-objects';
 import { canPurchaseCheck, transferNft } from './utils/productInteractions';
+import { formatShortenContract } from './utils/format';
 
 interface NFTCardProps {
   nft: any,
@@ -22,14 +23,14 @@ const NFTCard:React.FC<NFTCardProps> = ({ nft, product, ethPrice }) => {
   };
 
   return (
-    <div style={{ color: "black", backgroundColor: "white", borderRadius: "20px", width: "50%", padding: "1em", margin: "2rem" }}>
+    <div style={{ color: "black", backgroundColor: "white", borderRadius: "20px", width: "50%", padding: "1em", margin: "3.5rem" }}>
       <img alt="nft-img" width="60%" src={nft.image_url} />
       <p>Name: {nft.name}</p>
       <p>Description: {nft.description}</p>
       <p>Collection: {nft.collection.slug}</p>
-      <p>Contract: {nft.asset_contract.address}</p>
-      <p>tokenId: {nft.token_id}</p>
-      <button onClick={() => executeInstantBarter(nft, product, ethPrice)}>Barter Me</button>
+      <p>Contract: {formatShortenContract(nft.asset_contract.address)}</p>
+      <p>TokenId: {nft.token_id}</p>
+      <button className="sell-btn" onClick={() => executeInstantBarter(nft, product, ethPrice)}>Barter Me</button>
     </div>
   )
 }
