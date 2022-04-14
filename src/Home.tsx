32f5522cab5602f0d80aeae6a21269e1efeb5ac7
Collin Haney <collin.haney@yahoo.com>
2022-04-13 23:09:25 -0500

    Added 8 blocks on the home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -41,9 +41,78 @@ const Home = () => {
       </div>
 
 
-      <div>
-        {/* <h2 className="title">Welcome home</h2> */}
-        <p><a href="/checkout">Click here to view our product</a></p>
+      <div className="home-content">
+        {/* <p><a href="/checkout">Click here to view our product</a></p> */}
+        <div className="first-row">
+          <div className="home-box">
+            <h4>BOX 1</h4>
+            <div className="box-image">
+              <img/>
+            </div>
+            <p><a className="product-link" href="/checkout">Click here to view our product</a></p>
+          </div>
+
+          <div className="home-box">
+            <h4>BOX 2</h4>
+            <div className="box-image">
+              <img/>
+            </div>
+            <p><a className="product-link" href="/checkout">Click here to view our product</a></p>
+          </div>
+
+          <div className="home-box">
+            <h4>BOX 3</h4>
+            <div className="box-image">
+              <img/>
+            </div>
+            <p><a className="product-link" href="/checkout">Click here to view our product</a></p>
+          </div>
+
+          <div className="home-box">
+            <h4>BOX 4</h4>
+            <div className="box-image">
+              <img/>
+            </div>
+            <p><a className="product-link" href="/checkout">Click here to view our product</a></p>
+          </div>
+        </div>
+
+
+
+        <div className="second-row">
+          <div className="home-box">
+            <h4>BOX 5</h4>
+            <div className="box-image">
+              <img/>
+            </div>
+            <p><a className="product-link" href="/checkout">Click here to view our product</a></p>
+          </div>
+
+          <div className="home-box">
+            <h4>BOX 6</h4>
+            <div className="box-image">
+              <img/>
+            </div>
+            <p><a className="product-link" href="/checkout">Click here to view our product</a></p>
+          </div>
+
+          <div className="home-box">
+            <h4>BOX 7</h4>
+            <div className="box-image">
+              <img/>
+            </div>
+            <p><a className="product-link" href="/checkout">Click here to view our product</a></p>
+          </div>
+
+          <div className="home-box">
+            <h4>BOX 8</h4>
+            <div className="box-image">
+              <img/>
+            </div>
+            <p><a className="product-link" href="/checkout">Click here to view our product</a></p>
+          </div>
+        </div>
+
       </div>
     </body>
     
